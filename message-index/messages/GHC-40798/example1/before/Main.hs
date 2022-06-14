c8f5d93f6142ad4213c68df42ca92b6f3a5b6613
Carl Hedgren <Reasonable-Solutions@users.noreply.github.com>
2022-06-14 18:23:11 +0200

    Update message-index/messages/GHC-40798/example1/before/Main.hs
    Co-authored-by: PrettyPrincessKitty FS <thecommunistduck@hotmail.co.uk>

--- message-index/messages/GHC-40798/example1/before/Main.hs
+++ message-index/messages/GHC-40798/example1/before/Main.hs
@@ -4,8 +4,11 @@ module Main where
 
 main = print "notice the usage of whitespace below"
 
+f :: Num a => a -> a -> a
 f a b = a+ b -- this one is a warning
 
+g :: Num a => a -> a -> a
 g a b = a +b -- this one is also a warning
 
+h :: Num a => a -> a -> a
 h a b = a+b -- this one as well
