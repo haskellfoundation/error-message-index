145aa3687478b58aad762b7989d4ad0703045dfb
david-christiansen <david-christiansen@users.noreply.github.com>
2023-06-22 13:11:56 +0000

    deploy: b85a8128f1c1ce6f6b27f1928265bdf122d1c31f

---
+++ messages/GHC-87491/example1/after/ImportQualifiedPost.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module ImportQualifiedPost where
+
+import Prelude qualified
---
+++ messages/GHC-87491/example1/before/ImportQualifiedPost.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE Haskell2010 #-}
+
+module ImportQualifiedPost where
+
+import Prelude qualified
