134a161ce455f69b1db60812f6685f3bfa8d6d03
David Binder <david.binder@uni-tuebingen.de>
2024-06-01 19:40:36 +0200

    add filter_codes.sh script

--- index-cards/README.md
+++ index-cards/README.md
@@ -12,3 +12,6 @@ You can build the index cards by invoking `latexmk`.
 In a checkout of the GHC codebase, run `./hadrian/build lint:codes`.
 This builds the `lint-codes` executable which can emit useful information about all the Error messages that can be emitted.
 Then, use `_build/stage1/bin/lint-codes list > all_error_codes.txt` to generate the list of all error codes and write them to a file.
+
+Next, copy the file `testsuite/tests/diagnostic-codes/codes.stdout` from the GHC codebase. This file contains all the GHC error codes which have no corresponding test in the GHC testsuite. Remove the start of the file by hand.
+
---
+++ index-cards/filter_codes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script presumes that you have already followed the instructions to generate the file "all_error_codes.txt" from the GHC repository.
+
+# Create a list of all documented GHC error messages
+ls ../message-index/messages | grep GHC- > documented_error_codes.txt
+
+# Subtract from the list "all_error_codes" those which are already documented.
+grep -v -f documented_error_codes.txt all_error_codes.txt > undocumented_error_codes.txt
+
+# Subtract from this list all those error codes which are not tested in the GHC testsuite: It is unclear whether these error codes are reachable at all, so it is unclear if we should document them at all.
+awk '{print substr($1, 2, length($1) - 2)}' codes.stdout > untested_error_codes.txt
+grep -v -f untested_error_codes.txt undocumented_error_codes.txt > good_candidates.txt
+
+# Generate the content.tex from good_candidates.tex
+awk 'NF > 0 {print "\\ghcerror{" substr($1, 2, length($1) -2) "}"}' good_candidates.txt > content.tex
