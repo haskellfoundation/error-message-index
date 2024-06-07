8dbe4a8d2ce96293c6f355235629cb4b3dcc6144
Mauricio <mauzepeda@gmail.com>
2024-06-07 11:31:18 +0200

    Documented GHC-88333: Incorrect type variable on the LHS of infectivity condition

--- message-index/messages/GHC-88333/incorrect-type-var-injectivity-condition-type-family/after/Test.hs
+++ message-index/messages/GHC-88333/incorrect-type-var-injectivity-condition-type-family/after/Test.hs
@@ -4,9 +4,3 @@
 module IncorrectTypeVarLHSInjectivityCondition where
 
 type family F a = r | r -> a
-
--- Given input type a, there is exactly one output type r that the type family can map to
--- So, the uncommenting the lines below would be incorrect because it violates the injectivity condition
--- since the type family F would map both Char and Int to Double
--- type instance F Char = Double
--- type instance F Int = Double
