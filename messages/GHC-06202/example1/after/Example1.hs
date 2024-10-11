74c58188c5a2f228435f5dd4f12a89425cb45cd6
BinderDavid <BinderDavid@users.noreply.github.com>
2024-10-11 09:28:27 +0000

    deploy: 91ddc7330b2fc18d2a2fa5d101a7bb4413228a5b

---
+++ messages/GHC-06202/example1/after/Example1.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE Haskell2010, InstanceSigs #-}
+module Example1 where
+
+data A = A | B
+instance Eq A where
+    (==) :: A -> A -> Bool
+    A == A = True
+    B == B = True
+    _ == _ = False
